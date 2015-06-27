b7036eef84f555e6c42a9ab322d1e08a760d0345
Kevin321an <kevina321an.gmail.com>
2015-06-26 22:18:32 -0700

    at stage of 4.14, before delete the FetchWeather Task in Fragement file and implement the DB in APP

---
+++ app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
@@ -0,0 +1,7 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by FM on 6/21/2015.
+ */
+public class FetchWeatherTask {
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
@@ -0,0 +1,7 @@
+package com.example.android.sunshine.app.data;
+
+/**
+ * Created by FM on 6/21/2015.
+ */
+public class WeatherDbHelper {
+}
