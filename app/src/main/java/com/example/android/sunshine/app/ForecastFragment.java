a87a498c1bdf09acd2b6369ba3e56fbd8746b9f0
Kevin321an <kevin321an@gmail.com>
2015-09-01 21:39:45 -0700

    AppBar scrolling and the scorlling tracking with fragment() :design library

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -10,10 +10,12 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
@@ -316,6 +318,31 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
                             parallaxView.setTranslationY(Math.max(-max, parallaxView.getTranslationY() - dy / 2));
                         } else {//going down
                             parallaxView.setTranslationY(Math.min(0, parallaxView.getTranslationY() - dy / 2));
+                        }
+                    }
+                });
+            }
+        }
+
+        /**
+         * add little shadow between Appbar and recycle View.
+         */
+        final AppBarLayout appbarView = (AppBarLayout)rootView.findViewById(R.id.appbar);
+        if (null != appbarView) {
+            ViewCompat.setElevation(appbarView, 0);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                //!!!addOnScrollListener
+                mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                        if (0 == mRecyclerView.computeVerticalScrollOffset()) {
+                            appbarView.setElevation(0);
+                        } else {
+                            //getTargetElevation():
+                            // Returns the elevation value to use when this AppBarLayout should be elevated above content.
+                            appbarView.setElevation(appbarView.getTargetElevation());
                         }
                     }
                 });
